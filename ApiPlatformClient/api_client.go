d36f86b20bfdd739befd8c7de59101ae2670f9c9
h-alice <admin@halice.art>
2024-06-13 11:12:06 +0800

    HTTP basicauth crafter.

--- ApiPlatformClient/api_client.go
+++ ApiPlatformClient/api_client.go
@@ -1,5 +1,10 @@
 package api_platform_client
 
+import (
+	"encoding/base64"
+	"fmt"
+)
+
 // # API Platform Client Struct
 //
 // This struct is used to store the API platform client information.
@@ -9,4 +14,12 @@ type ApiPlatformClient struct {
 	ClientTokenBlock string
 	accessToken      string // `accessToken` is generated during runtime.
 	signBlock        string // `signBlock` is generated during runtime.
+}
+
+// # Payload Crafter for HTTP Basic Auth
+//
+// This function is used to craft the payload for HTTP Basic Auth.
+func basicAuthCredentialCrafter(clientID, clientTokenBlock string) string {
+	credential := fmt.Sprintf("%s:%s", clientID, clientTokenBlock)
+	return base64.StdEncoding.EncodeToString([]byte(credential))
 }
