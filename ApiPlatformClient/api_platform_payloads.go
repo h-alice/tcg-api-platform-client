e9ed64b258b663eabaa0550cb4cbb1e69f0a943c
h-alice <admin@halice.art>
2024-06-15 11:19:40 +0800

    Add response payload definition.

--- ApiPlatformClient/api_platform_payloads.go
+++ ApiPlatformClient/api_platform_payloads.go
@@ -1,1 +1,10 @@
 package api_platform_client
+
+type ApiPlatformTokenResponse struct {
+	AccessToken string
+	TokenType   string
+	Expires     int
+	Scope       string
+	Node        string
+	Jti         string // Unknown
+}
