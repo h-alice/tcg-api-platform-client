f50684891088ee3ec06f351647dcd11dbf7d6261
h-alice <admin@halice.art>
2024-06-16 11:21:02 +0800

    Add json tags.

--- ApiPlatformClient/api_platform_payloads.go
+++ ApiPlatformClient/api_platform_payloads.go
@@ -1,10 +1,10 @@
 package api_platform_client
 
 type ApiPlatformTokenResponse struct {
-	AccessToken string
-	TokenType   string
-	Expires     int
-	Scope       string
-	Node        string
-	Jti         string // Unknown
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	Expires     int    `json:"expires_in"`
+	Scope       string `json:"scope"`
+	Node        string `json:"node"`
+	Jti         string `json:"jti"` // Unknown
 }
